4ef901f4fdb6915b8709f04db696c8c3e41b7164
Praktikant <kimhep@gmx.de>
2018-01-10 08:18:58 +0100

    Richtig gerückt

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<?php include("head.html");?>
+<body>
+
+<?php include("navigation.html");?>
+
+        <main class="container">
+            <div class="jumbotron orange-font">
+                <h1>Mein Praktikumsbericht</h1>
+                <p>Auf dieser Webiste kannst du sehen, wie ich mit Hilfe von "bootstrap" meine Website verbessert habe.</p>
+
+            </div>
+
+            <div class="tab-content">
+                <div id="home" class="container tab-pane active"><br>
+                    <h3>Startseite</h3>
+                    <p>Hallo! Hier zeige ich, was ich in den 2 Wochen bei meinem Praktikum gelernt habe</p>
+                </div>
+                <div id="menu1" class="container tab-pane fade"><br>
+                    <h3>Erste Woche</h3>
+                    <p></p>
+                </div>
+                <div id="menu2" class="container tab-pane fade"><br>
+                    <h3>Zweite Woche</h3>
+                    <p></p>
+                </div>
+                <div id="menu3" class="container tab-pane fade"><br>
+                    <h3>Twitter</h3>
+
+                </div>
+            </div>
+        </main>
+        <?php include("scripts.html");?>
+    </body>
+</html>
---
+++ twitter.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <?php include("head.html");?>
+    <body>
+        <?php include("navigation.html");?>
+
+        <main>
+            <div class="jumbotron orange-font">
+            <h1>Mein Praktikumsbericht</h1>
+            <p>Auf dieser Webiste kannst du sehen, wie ich mit Hilfe von "bootstrap" meine Website verbessert habe.</p>
+            </div>
+            <a class="twitter-timeline" href="https://twitter.com/BffBlatt?ref_src=twsrc%5Etfw"></a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </main>
+
+        <?php include("scripts.html");?>
+    </body>
+</html>
