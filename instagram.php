cf6a9d4fae1dae09cb01aa0b07c949e5d49e70fe
Praktikant <kimhep@gmx.de>
2018-01-19 14:25:59 +0100

    font-family/size hinzugefügt

--- instagram.php
+++ instagram.php
@@ -6,13 +6,15 @@
 <style>
     .jumbotron {
         color: #6233a0;
+        font-family: 'EB Garamond', serif;
+        font-size: large;
     }
 </style>
 
 <main>
     <div class="jumbotron">
         <h1>Instagram</h1>
-        <p>Hier siehst du die Posts meiner Lieblingsinstagramer.</p>
+        <p>Hier siehst du die Posts meines Lieblingsinstagramer.</p>
         <a href="https://instawidget.net/v/user/wisdomcoach" id="link-1659785f476e41061a5b27a4c8b224da7193cd4ef9a2fd7fe5e1465c9fbc5abd">@wisdomcoach</a>
         <script src="https://instawidget.net/js/instawidget.js?u=1659785f476e41061a5b27a4c8b224da7193cd4ef9a2fd7fe5e1465c9fbc5abd&width=300px"></script>
     </div>
