04547fb4fc2edab5391ce17d735a50d88c517509
Praktikant <kimhep@gmx.de>
2018-01-19 09:22:05 +0100

    Ein Widget entfernt

--- instagram.php
+++ instagram.php
@@ -15,9 +15,6 @@
         <p>Hier siehst du die Posts meiner Lieblingsinstagramer.</p>
         <a href="https://instawidget.net/v/user/wisdomcoach" id="link-1659785f476e41061a5b27a4c8b224da7193cd4ef9a2fd7fe5e1465c9fbc5abd">@wisdomcoach</a>
         <script src="https://instawidget.net/js/instawidget.js?u=1659785f476e41061a5b27a4c8b224da7193cd4ef9a2fd7fe5e1465c9fbc5abd&width=300px"></script>
-        <!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe
-            src="//lightwidget.com/widgets/0e00e474fb2c5870a2e3ce1280dde3b0.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
-        <a href="https://instawidget.net/v/user/wisdomcoach" id="link-1659785f476e41061a5b27a4c8b224da7193cd4ef9a2fd7fe5e1465c9fbc5abd">@wisdomcoach</a>
     </div>
 
 
