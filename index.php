0ae4674d38b4be1632be0ac13b277245ee5e0c57
Praktikant <kimhep@gmx.de>
2018-01-19 09:21:46 +0100

    Text überarbeitet

--- index.php
+++ index.php
@@ -14,28 +14,16 @@
         <main class="container">
             <div class="jumbotron ">
                 <h1>Mein Praktikumsbericht</h1>
-                <p>Auf dieser Webiste kannst du sehen, wie ich mit Hilfe von "bootstrap" meine Website verbessert habe.</p>
 
             </div>
 
             <div class="tab-content">
                 <div id="home" class="container tab-pane active"><br>
                     <h3>Startseite</h3>
-                    <p>Hallo! Hier zeige ich, was ich in den 2 Wochen bei meinem Praktikum gelernt habe</p>
+                    <p>Hallo, hier kannst du sehen, was ich innerhalb der 2 Wochen Praktikum alles gelernt habe. Zudem kannst du dir auch auch mein <a href="https://github.com/kimhep/webfactoryPraktikum">Git</a> anschauen um zu sehen, wie der Code aussiehst.</p>
                 </div>
-                <div id="menu1" class="container tab-pane fade"><br>
-                    <h3>Erste Woche</h3>
-                    <p></p>
-                </div>
-                <div id="menu2" class="container tab-pane fade"><br>
-                    <h3>Zweite Woche</h3>
-                    <p></p>
-                </div>
-                <div id="menu3" class="container tab-pane fade"><br>
-                    <h3>Twitter</h3>
 
                 </div>
-            </div>
         </main>
     </body>
 </html>
